51e2d164e57be91f7102847c4e4bb00782ee4932
Peter Yaacoub <yaapete.dev@gmail.com>
2021-12-17 14:42:02 +0200

    🐞 Fix presentedViewController's value
    When the root view controller wasn't presenting another view controller, the share sheet was not appearing. Therefore, the controller now falls back on the window's root view controller. The share sheet now also works through the Playground.

--- Present share sheet in SwiftUI.playground/Contents.swift
+++ Present share sheet in SwiftUI.playground/Contents.swift
@@ -32,12 +32,9 @@ extension View {
     func shareSheet(isPresented: Binding<Bool>, items: [Any]) -> some View {
         guard isPresented.wrappedValue else { return self }
         let activityViewController = UIActivityViewController(activityItems: items, applicationActivities: nil)
+        let presentedViewController = UIApplication.keyWindow?.rootViewController?.presentedViewController ?? UIApplication.keyWindow?.rootViewController
         activityViewController.completionWithItemsHandler = { _, _, _, _ in isPresented.wrappedValue = false }
-        guard let presentedViewController = UIApplication.keyWindow?.rootViewController?.presentedViewController else {
-            print("The presented view controller is not available through the Playground")
-            return self
-        }
-        presentedViewController.present(activityViewController, animated: true)
+        presentedViewController?.present(activityViewController, animated: true)
         return self
     }
     
